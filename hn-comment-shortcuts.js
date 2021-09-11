8cc6e2f2af9676a8dcfb5d11c90d58e03d0df754
Christian Jauvin <cjauvin@gmail.com>
2021-09-11 14:04:51 -0400

    Use prettier

--- hn-comment-shortcuts.js
+++ hn-comment-shortcuts.js
@@ -1,34 +1,46 @@
-console.log('HN extension');
+console.log("HN extension");
 
-let result = document.evaluate('//tr[contains(@class, "athing") and contains(@class, "comtr")]', document, null, XPathResult.ANY_TYPE, null);
+let result = document.evaluate(
+  '//tr[contains(@class, "athing") and contains(@class, "comtr")]',
+  document,
+  null,
+  XPathResult.ANY_TYPE,
+  null
+);
 
-let elem, elems = []; // elems is a list of <tr> nodes
-while (elem = result.iterateNext()) {
+let elem,
+  elems = []; // elems is a list of <tr> nodes
+while ((elem = result.iterateNext())) {
   elems.push(elem);
 }
 
-let comments = {'0': {elem: null, parent: null, children: []}}; // id -> {elem, parent, children}
+let comments = { "0": { elem: null, parent: null, children: [] } }; // id -> {elem, parent, children}
 
 for (elem of elems) {
-  comments[elem.id] = {elem, parent: null, children: []};
+  comments[elem.id] = { elem, parent: null, children: [] };
 }
 
-let levelToId = {'-1': 0}; // level -1 is set to root
+let levelToId = { "-1": 0 }; // level -1 is set to root
 
 for (elem of elems) {
-  let ind = document.evaluate('.//td[@class = "ind"]', elem, null, XPathResult.ANY_TYPE, null).iterateNext();
-  let level = ind.getAttribute('indent');
+  let ind = document
+    .evaluate('.//td[@class = "ind"]', elem, null, XPathResult.ANY_TYPE, null)
+    .iterateNext();
+  let level = ind.getAttribute("indent");
   levelToId[level] = elem.id;
   // find the parent on the level above the current one
   let parent = levelToId[parseInt(level) - 1];
   comments[elem.id].parent = parent;
   comments[parent].children.push(elem.id);
 
-  comments[elem.id].elem.addEventListener('click', function(elemId) {
-    return function() {
-      setCurrentComment(elemId);
-    }
-  }(elem.id));
+  comments[elem.id].elem.addEventListener(
+    "click",
+    (function (elemId) {
+      return function () {
+        setCurrentComment(elemId);
+      };
+    })(elem.id)
+  );
 
   // comments[elem.id].elem.addEventListener('dblclick', function(elemId) {
   //   return function(e) {
@@ -37,7 +49,6 @@ for (elem of elems) {
   //     e.preventDefault();
   //   }
   // }(elem.id));
-
 }
 
 // This is needed to deal with negative numbers
@@ -57,10 +68,12 @@ function setCurrentComment(newId) {
   currId = newId;
 }
 
+function findNextComment() {}
+
 let currId = comments[0].children[0];
 setCurrentComment(currId);
 
-document.addEventListener('keydown', function(e) {
+document.addEventListener("keydown", function (e) {
   if (e.key === "n" || e.key === "p") {
     let siblings = comments[comments[currId].parent].children;
     let idx = siblings.indexOf(currId);
@@ -75,9 +88,12 @@ document.addEventListener('keydown', function(e) {
   } else if (e.key === "l") {
     let isCollapsed = comments[currId].elem.className.includes("coll");
     if (!isCollapsed && comments[currId].children.length > 0) {
+      // Child comment
       setCurrentComment(comments[currId].children[0]);
       comments[currId].elem.scrollIntoView(false);
     } else {
+      findNextComment();
+
       let siblings = comments[comments[currId].parent].children;
       let idx = siblings.indexOf(currId);
       if (idx < siblings.length - 1) {
@@ -86,7 +102,15 @@ document.addEventListener('keydown', function(e) {
       }
     }
   } else if (e.key === "Tab") {
-    let togg = document.evaluate('.//a[@class = "togg"]', comments[currId].elem, null, XPathResult.ANY_TYPE, null).iterateNext();
+    let togg = document
+      .evaluate(
+        './/a[@class = "togg"]',
+        comments[currId].elem,
+        null,
+        XPathResult.ANY_TYPE,
+        null
+      )
+      .iterateNext();
     togg.click();
     e.preventDefault();
   }
