3bd845503fbc7aa8407c9478e8f0e7b21aa50975
Christian Jauvin <cjauvin@gmail.com>
2021-09-11 14:49:21 -0400

    Find next comment

--- hn-comment-shortcuts.js
+++ hn-comment-shortcuts.js
@@ -14,13 +14,14 @@ while ((elem = result.iterateNext())) {
   elems.push(elem);
 }
 
+// The ID keys in this table should remain strings, to avoid confusion
 let comments = { "0": { elem: null, parent: null, children: [] } }; // id -> {elem, parent, children}
 
 for (elem of elems) {
   comments[elem.id] = { elem, parent: null, children: [] };
 }
 
-let levelToId = { "-1": 0 }; // level -1 is set to root
+let levelToId = { "-1": "0" }; // level -1 is set to root
 
 for (elem of elems) {
   let ind = document
@@ -37,7 +38,7 @@ for (elem of elems) {
     "click",
     (function (elemId) {
       return function () {
-        setCurrentComment(elemId);
+        currId = setCurrentComment(elemId);
       };
     })(elem.id)
   );
@@ -65,13 +66,28 @@ function setCurrentComment(newId) {
   // We modify the border of the <td> child of the comment <tr>
   comments[currId].elem.firstElementChild.style.border = "";
   comments[newId].elem.firstElementChild.style.border = "thin dashed red";
-  currId = newId;
+  return newId;
 }
 
-function findNextComment() {}
+function findNextComment(id) {
+  if (id === "0") {
+    // Special case
+    currId = setCurrentComment(comments["0"].children[0]);
+    comments[currId].elem.scrollIntoView(false);
+    return;
+  }
+  let siblings = comments[comments[id].parent].children;
+  let idx = siblings.indexOf(id);
+  if (idx < siblings.length - 1) {
+    currId = setCurrentComment(siblings[idx + 1]);
+    comments[currId].elem.scrollIntoView(false);
+  } else {
+    findNextComment(comments[id].parent);
+  }
+}
 
-let currId = comments[0].children[0];
-setCurrentComment(currId);
+let currId = comments["0"].children[0];
+currId = setCurrentComment(currId);
 
 document.addEventListener("keydown", function (e) {
   if (e.key === "n" || e.key === "p") {
@@ -79,27 +95,20 @@ document.addEventListener("keydown", function (e) {
     let idx = siblings.indexOf(currId);
     idx = idx + (e.key === "n" ? 1 : -1);
     idx = mod(idx, siblings.length);
-    setCurrentComment(siblings[idx]);
+    currId = setCurrentComment(siblings[idx]);
     comments[currId].elem.scrollIntoView(false);
   } else if (e.key === "h") {
-    if (comments[currId].parent) {
-      setCurrentComment(comments[currId].parent);
+    if (comments[currId].parent !== "0") {
+      currId = setCurrentComment(comments[currId].parent);
     }
   } else if (e.key === "l") {
     let isCollapsed = comments[currId].elem.className.includes("coll");
     if (!isCollapsed && comments[currId].children.length > 0) {
       // Child comment
-      setCurrentComment(comments[currId].children[0]);
+      currId = setCurrentComment(comments[currId].children[0]);
       comments[currId].elem.scrollIntoView(false);
     } else {
-      findNextComment();
-
-      let siblings = comments[comments[currId].parent].children;
-      let idx = siblings.indexOf(currId);
-      if (idx < siblings.length - 1) {
-        setCurrentComment(siblings[idx + 1]);
-        comments[currId].elem.scrollIntoView(false);
-      }
+      findNextComment(currId);
     }
   } else if (e.key === "Tab") {
     let togg = document
